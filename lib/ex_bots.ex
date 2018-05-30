c2a3ea6946a49090a1e402e60aa89c7659f1d652
Jeff Smith <jeff@sohlsmith.com>
2018-05-29 23:34:54 -0500

    Add RSS collector for phys.org.

--- lib/ex_bots.ex
+++ lib/ex_bots.ex
@@ -8,7 +8,8 @@ defmodule ExBots do
       worker(ExBots.Brain, []),
       worker(ExBots.Astrobot, []),
       worker(ExBots.RSS.Collector, ["#bottest", "https://www.universetoday.com/feed/", :ets.new(nil, [:public])], id: UTCollector),
-      worker(ExBots.RSS.Collector, ["#bottest", "https://spaceflightnow.com/feed/", :ets.new(nil, [:public])], id: SFNCollector)
+      worker(ExBots.RSS.Collector, ["#bottest", "https://spaceflightnow.com/feed/", :ets.new(nil, [:public])], id: SFNCollector),
+      worker(ExBots.RSS.Collector, ["#bottest", "https://phys.org/rss-feed/space-news/", :ets.new(nil, [:public])], id: PhysCollector)
     ]
 
     opts = [strategy: :one_for_one, name: ExBots.Supervisor]
