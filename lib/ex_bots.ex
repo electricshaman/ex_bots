feb66f40c4545f82fe731c7697553312eefe1fc8
Jeff Smith <jeff@sohlsmith.com>
2018-05-31 00:10:13 -0500

    Remove SpaceflightNow RSS collection.

--- lib/ex_bots.ex
+++ lib/ex_bots.ex
@@ -8,7 +8,6 @@ defmodule ExBots do
       worker(ExBots.Brain, []),
       worker(ExBots.Astrobot, []),
       worker(ExBots.RSS.Collector, ["#bottest", "https://www.universetoday.com/feed/", :ets.new(nil, [:public])], id: UTCollector),
-      worker(ExBots.RSS.Collector, ["#bottest", "https://spaceflightnow.com/feed/", :ets.new(nil, [:public])], id: SFNCollector),
       worker(ExBots.RSS.Collector, ["#bottest", "https://phys.org/rss-feed/space-news/", :ets.new(nil, [:public])], id: PhysCollector)
     ]
 
